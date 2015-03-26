47a936c9ccab912e452b5f2f9deda383177d3c42
Edgars Beigarts <edgars.beigarts@gmail.com>
2015-03-27 00:19:58 +0200

    Simplify next column generator

--- lib/saxlsx/column_name_generator.rb
+++ lib/saxlsx/column_name_generator.rb
@@ -1,34 +1,17 @@
 module Saxlsx
   class ColumnNameGenerator
-
     FIRST = 'A'
-    LAST = 'Z'
+    LAST  = 'Z'
 
     def self.next_to(previous)
-      cache previous do
-        parts = (previous || '').chars.to_a
-        char = parts.pop
-
-        if char.nil? || char.empty?
-          FIRST
-        elsif char < LAST
-          parts << (char.ord + 1).chr
-          parts.join
-        else
-          "#{next_to(parts.join)}A"
-        end
+      char = previous ? previous[-1] : nil
+      if char.nil?
+        FIRST
+      elsif char < LAST
+        previous[0..-2] + (char.ord + 1).chr
+      else
+        "#{next_to(previous[0..-2])}A"
       end
     end
-
-    private
-
-    def self.cache(key, &block)
-      @cache ||= {}
-
-      return @cache[key] if @cache.has_key? key
-
-      @cache[key] = block.call
-    end
-
   end
 end
